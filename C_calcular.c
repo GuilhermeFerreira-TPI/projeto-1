ee0a2938abce73aeb877055345ad902d7a25a6e1
Project GEA <guilhermeladeiro@gmail.com>
2025-10-23 09:15:56 +0100

    Criar função soma

--- C_calcular.c
+++ C_calcular.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+
+int soma(int n1, int n2){
+    return n1 + n2;}
+
 int main(){
 
     int n1, n2;
+    int resultado;
     printf("Insira dois numeros inteiros: ");
     scanf("%d %d", &n1, &n2);
  
+    resultado = soma(n1, n2);
+    printf("A soma de %d e %d e: %d\n", n1, n2, resultado);
     return 0;
 }
