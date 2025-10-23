c60617044d8918aab595601fa08a1fd92ad32f15
Project GEA <guilhermeladeiro@gmail.com>
2025-10-23 10:00:03 +0100

    Criar Função Dividir e chamar a mesma

--- C_calcular.c
+++ C_calcular.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-int soma(int n1, int n2){
+float soma(float n1, float n2){
     return n1 + n2;};
 
-int subtrair(int n1, int n2){
+float subtrair(float n1,float n2){
     return n1 - n2;};
 
-int multiplicar(int n1, int n2){
+float multiplicar(float n1, float n2){
     return n1 * n2;};
+
+float dividir(float n1, float n2){
+    if (n2 != 0){
+        return n1 / n2;} 
+    else{
+        printf("Erro: Divisao por zero!\n");
+        return 0;}}
 
 int main(){
 
-    int n1, n2;
-    int resultado;
+    float n1, n2;
     printf("Insira dois numeros inteiros: ");
-    scanf("%d %d", &n1, &n2);
+    scanf("%f %f", &n1, &n2);
  
-    printf("A soma de %d e %d e: %d\n", n1, n2,soma(n1, n2));
-    printf("A subtracao de %d e %d e: %d\n", n1, n2, subtrair(n1, n2));
-    printf("A multiplicacao de %d e %d e: %d\n", n1, n2, multiplicar(n1, n2));
+    printf("A soma de %.2f e %.2f e: %.2f\n", n1, n2, soma(n1, n2));
+    printf("A subtracao de %.2f e %.2f e: %.2f\n", n1, n2, subtrair(n1, n2));
+    printf("A multiplicacao de %.2f e %.2f e: %.2f\n", n1, n2, multiplicar(n1, n2));
+    printf("A divisao de %.2f e %.2f e: %.2f\n", n1, n2, dividir(n1, n2));
 }
